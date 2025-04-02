0ca87cb1702e30f9a1ce1ca13766dab7d328edb6
Devarsh14 <d.shukla@vald.com>
2025-04-02 10:49:04 +1000

    project 2 simple group generator

---
+++ project2GroupNameGenerator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to group name generator")
+color = input("What's your favorite colour?\n")
+animal = input("What's your favorite Animal?\n")
+print(f"your group name could be {color} {animal}s")
