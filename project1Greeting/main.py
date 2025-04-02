a5eb0c8bff04e8fbea451609369090b6063a080c
Devarsh14 <d.shukla@vald.com>
2025-04-02 10:39:08 +1000

    first python command

---
+++ project1Greeting/main.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+print(f"{name}, how is your day? Welocme to the Python!")
+
+# This is a simple greeting program that asks for the user's name and prints a greeting message.
---
+++ readme.md
@@ -0,0 +1,4 @@
+#miniconda setup
+conda create -p ./myenv python=3.12 -c conda-forge
+conda activate ./myenv
+conda deactivate
